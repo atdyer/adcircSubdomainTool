11556f213de94792681896a5931b5fecc3290122
atdyer <atdyer@ncsu.edu>
2013-08-02 15:18:49 -0400

    Started writing the RectangleTool class

--- SubdomainTools/RectangleTool.cpp
+++ SubdomainTools/RectangleTool.cpp
@@ -2,4 +2,138 @@
 
 RectangleTool::RectangleTool()
 {
+	terrain = 0;
+	camera = 0;
+
+	VAOId = 0;
+	VBOId = 0;
+	IBOId = 0;
+	fillShader = 0;
+
+	selectionMode = elementSelection;
+
+	firstCornerPixel[0] = 0.0;
+	firstCornerPixel[1] = 0.0;
+	secondCornerPixel[0] = 0.0;
+	secondCornerPixel[1] = 0.0;
+	firstCornerNormal[0] = 0.0;
+	firstCornerNormal[1] = 0.0;
+	secondCornerNormal[0] = 0.0;
+	secondCornerNormal[1] = 0.0;
+	firstCornerDomain[0] = 0.0;
+	firstCornerDomain[1] = 0.0;
+	secondCornerDomain[0] = 0.0;
+	secondCornerDomain[1] = 0.0;
+
+	w = 800;
+	h = 600;
+	l = -1.0;
+	r = 1.0;
+	b = -1.0;
+	t = 1.0;
+}
+
+
+RectangleTool::~RectangleTool()
+{
+	/* Clean up shader */
+	if (fillShader)
+		delete fillShader;
+
+	/* Clean up OpenGL stuff */
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+	if (VAOId)
+		glDeleteBuffers(1, &VAOId);
+	if (VBOId)
+		glDeleteBuffers(1, &VBOId);
+	if (IBOId)
+		glDeleteBuffers(1, &IBOId);
+}
+
+
+void RectangleTool::Draw()
+{
+
+}
+
+
+void RectangleTool::InitializeGL()
+{
+
+}
+
+
+void RectangleTool::SetCamera(GLCamera *cam)
+{
+	camera = cam;
+}
+
+
+void RectangleTool::SetViewportSize(float w, float h)
+{
+	this->w = w;
+	this->h = h;
+	this->l = -1.0*w/h;
+	this->r = 1.0*w/h;
+	this->b = -1.0;
+	this->t = 1.0;
+}
+
+
+void RectangleTool::SetFirstCorner(int newX, int newY)
+{
+	firstCornerPixel[0] = newX;
+	firstCornerPixel[1] = newY;
+
+	if (camera)
+		camera->GetUnprojectedPoint(firstCornerPixel[0], firstCornerPixel[1], &firstCornerNormal[0], &firstCornerNormal[1]);
+	else
+		DEBUG("Rectangle Tool: No Camera");
+
+	if (terrain)
+	{
+		firstCornerDomain[0] = terrain->GetUnprojectedX(firstCornerNormal[0]);
+		firstCornerDomain[1] = terrain->GetUnprojectedY(firstCornerNormal[1]);
+	} else {
+		DEBUG("Rectangle Tool: No Terrain");
+	}
+}
+
+
+void RectangleTool::SetSecondCorner(int newX, int newY)
+{
+	secondCornerPixel[0] = newX;
+	secondCornerPixel[1] = newY;
+
+	if (camera)
+		camera->GetUnprojectedPoint(secondCornerPixel[0], secondCornerPixel[1], &secondCornerNormal[0], &secondCornerNormal[1]);
+	else
+		DEBUG("Rectangle Tool: No Camera");
+
+	if (terrain)
+	{
+		secondCornerDomain[0] = terrain->GetUnprojectedX(secondCornerNormal[0]);
+		secondCornerDomain[1] = terrain->GetUnprojectedY(secondCornerNormal[1]);
+	} else {
+		DEBUG("Rectangle Tool: No Terrain");
+	}
+}
+
+
+void RectangleTool::RectangleFinished()
+{
+
+}
+
+
+void RectangleTool::SetTerrainLayer(TerrainLayer *layer)
+{
+	terrain = layer;
+}
+
+
+void RectangleTool::SetSelectionMode(SelectionType newMode)
+{
+	selectionMode = newMode;
 }
--- SubdomainTools/RectangleTool.h
+++ SubdomainTools/RectangleTool.h
@@ -1,10 +1,81 @@
 #ifndef RECTANGLETOOL_H
 #define RECTANGLETOOL_H
 
-class RectangleTool
+#include <QObject>
+#include <string>
+
+#include "adcData.h"
+#include "OpenGL/GLCamera.h"
+#include "Layers/TerrainLayer.h"
+
+
+enum SelectionType {nodeSelection, elementSelection};
+
+
+class RectangleTool : public QObject
 {
+		Q_OBJECT
 	public:
 		RectangleTool();
+		~RectangleTool();
+
+		void	Draw();
+		void	InitializeGL();
+		void	SetCamera(GLCamera* cam);
+		void	SetViewportSize(float w, float h);
+		void	SetFirstCorner(int newX, int newY);
+		void	SetSecondCorner(int newX, int newY);
+		void	RectangleFinished();
+
+		std::vector<Element*>	GetSelectedElements();
+
+	protected:
+
+		TerrainLayer*	terrain;	/**< The TerrainLayer that nodes/elements will be selected from */
+		GLCamera*	camera;		/**< The GLCamera that is used to draw the TerrainLayer */
+
+		/* OpenGL Stuff */
+		bool	glLoaded;	/**< Flag that shows if the VAO/VBO/IBO have been created */
+		GLuint	VAOId;		/**< The vertex array object ID */
+		GLuint	VBOId;		/**< The vertex buffer object ID */
+		GLuint	IBOId;		/**< The index buffer object ID */
+		SolidShader*	fillShader;	/**< The shader used to draw the selection tool */
+
+		/* Selection Mode */
+		SelectionType	selectionMode;	/**< The current selection mode */
+
+		/* Selected Nodes/Elements */
+		std::vector<Node*>	selectedNodes;
+		std::vector<Element*>	selectedElements;
+
+		/* Rectangle Attributes */
+		float	firstCornerPixel[2];	/**< The coordinates of the first corner of the rectangle, in pixels */
+		float	secondCornerPixel[2];	/**< The coordinates of the second corner of the rectangle, in pixels */
+
+		float	firstCornerNormal[2];	/**< The coordinates of the first corner of the rectangle, in normalized OpenGL space */
+		float	secondCornerNormal[2];	/**< The coordinates of the second corner of the rectangle, in normalized OpenGL space */
+
+		float	firstCornerDomain[2];	/**< The coordinates of the first corner of the rectangle, in the original coordinate system of TerrainLayer */
+		float	secondCornerDomain[2];	/**< The coordinates of the second corner of the rectangle, in the original coordinate system of TerrainLayer */
+
+		/* Viewport Attributes */
+		float	w;	/**< The viewport width */
+		float	h;	/**< The viewport height */
+		float	l;	/**< The left side of the viewport */
+		float	r;	/**< The right side of the viewport */
+		float	b;	/**< The bottom of the viewport */
+		float	t;	/**< The top of the viewport */
+
+	signals:
+
+		void	RectangleStatsSet(float w, float h);
+		void	RectangleStatsFinished();
+		void	FinishedSearching();
+
+	public slots:
+
+		void	SetTerrainLayer(TerrainLayer *layer);
+		void	SetSelectionMode(SelectionType newMode);
 };
 
 #endif // RECTANGLETOOL_H
